4d96a4a7a78d83f4b68d823a4195a2b5cd196276
Haynes Mathew George <hgeorge@codeaurora.org>
2011-12-01 17:25:51 -0800

    libstagefright: Ignore bad size error when parsing d263 atom.
    Change-Id: Id71c6d55181d46671aba3e14bfbc4933687b180f
    CRs-Fixed: 288020

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -1341,8 +1341,14 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             char buffer[23];
             if (chunk_data_size != 7 &&
                 chunk_data_size != 23) {
+#ifdef QCOM_HARDWARE
+                LOGW("Incorrect D263 box size %lld, skipping this atom", chunk_data_size);
+                *offset += chunk_size;
+                break;
+#else
                 LOGE("Incorrect D263 box size %lld", chunk_data_size);
                 return ERROR_MALFORMED;
+#endif
             }
 
             if (mDataSource->readAt(
